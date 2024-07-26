da3111612f1fc085d6458312a696e20569d2603a
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-07-25 18:25:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,21 @@ form_url = "https://docs.google.com/forms/d/e/1FAIpQLSfz9RyOoxql_5ULHpthE33PcAVJ
 # URL de tu Google Sheet
 sheet_url = "https://docs.google.com/spreadsheets/d/1OsShDjCOCM0OgPBMYh1LMKgrLw5qgLg8s2ol6oIr3r0/edit?resourcekey=&gid=132221002#gid=132221002"
 
+# Configuración de la barra lateral
+with st.sidebar:
+    st.header("Ayuda")
+    st.markdown("""
+        ## Instrucciones
+        
+        **Formulario de Google:**
+        Completa el formulario incrustado a continuación para enviar tu información. Asegúrate de completar todos los campos requeridos.
+
+        **Hoja de Cálculo:**
+        [Accede a la hoja de cálculo aquí](https://docs.google.com/spreadsheets/d/1OsShDjCOCM0OgPBMYh1LMKgrLw5qgLg8s2ol6oIr3r0/edit?resourcekey=&gid=132221002#gid=132221002) para ver los datos registrados.
+
+        Si tienes alguna pregunta, por favor contacta al soporte.
+    """)
+
 # Incrustar el formulario en Streamlit
 st.title("Formulario de Google en Streamlit")
 components.iframe(form_url, width=800, height=600, scrolling=True)
-
-# Agregar un enlace a Google Sheet
-st.markdown(f"[Accede a la hoja de cálculo aquí]({sheet_url})", unsafe_allow_html=True)

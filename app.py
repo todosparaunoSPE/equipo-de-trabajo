53f110b390b3db78cd1bfb8a77f68433b264bf83
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-07-25 18:19:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # URL del formulario de Google Forms
-form_url = "https://docs.google.com/forms/d/e/1FAIpQLSfz9RyOoxql_5ULHpthE33PcAVJOAn9wZBSa32SapsBzYkRFQ/viewform?usp=sf_link/viewform"
+form_url = "https://docs.google.com/forms/d/e/1FAIpQLSfz9RyOoxql_5ULHpthE33PcAVJOAn9wZBSa32SapsBzYkRFQ/viewform?usp=sf_link"
+
+# URL de tu Google Sheet
+sheet_url = "https://docs.google.com/spreadsheets/d/1OsShDjCOCM0OgPBMYh1LMKgrLw5qgLg8s2ol6oIr3r0/edit?resourcekey=&gid=132221002#gid=132221002"
 
 # Incrustar el formulario en Streamlit
 st.title("Formulario de Google en Streamlit")
 components.iframe(form_url, width=800, height=600, scrolling=True)
 
 # Agregar un enlace a Google Sheet
-st.markdown(f"[Accede a la hoja de cálculo aquí]({https://docs.google.com/spreadsheets/d/1OsShDjCOCM0OgPBMYh1LMKgrLw5qgLg8s2ol6oIr3r0/edit?resourcekey=&gid=132221002#gid=132221002})", unsafe_allow_html=True)
+st.markdown(f"[Accede a la hoja de cálculo aquí]({sheet_url})", unsafe_allow_html=True)
